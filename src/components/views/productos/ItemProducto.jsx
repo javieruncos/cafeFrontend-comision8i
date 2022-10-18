65054ef032e3f71c6ab8bf53fe69241406599104
Javier Uncos <javiersrfc30@gmail.com>
2022-10-18 13:34:07 -0300

    agregamos el alert a borrarProducto

--- src/components/views/productos/ItemProducto.jsx
+++ src/components/views/productos/ItemProducto.jsx
@@ -4,25 +4,50 @@ import Swal from "sweetalert2";
 import { borrarProductoApi, consultarApi } from "../../../helper/queries";
 import { Link } from "react-router-dom";
 
-const ItemProducto = ({producto,setProductos}) => {
+const ItemProducto = ({ producto, setProductos }) => {
+  const borrarProducto = () => {
+    Swal.fire({
+      title: 'Estas seguro que quieres eliminar este producto?',
+      text: "No se puede revertir este paso",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'SI, Eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
 
-  const borrarProducto =()=>{
-    borrarProductoApi(producto.id).then((respuesta)=>{
-       if(respuesta.status === 200){
-           Swal.fire("producto eliminado","el producto fue exitosamente borrado","success")
-          //  busco todos los productos existentes en ese momento de tiempo 
-          consultarApi().then((respuesta)=>{
-            // actualizo el state de productos de Administrador con los daros que hay en la API
-            setProductos(respuesta)
-          })
-       }else{
-        Swal.fire("ocurrio un error","intente esta operacion mas tarde","error")
-       }
+        borrarProductoApi(producto.id).then((respuesta) => {
+          if (respuesta.status === 200) {
+            Swal.fire(
+              "producto eliminado",
+              "el producto fue exitosamente borrado",
+              "success"
+            );
+            //  busco todos los productos existentes en ese momento de tiempo
+            consultarApi().then((respuesta) => {
+              // actualizo el state de productos de Administrador con los daros que hay en la API
+              setProductos(respuesta);
+            });
+          } else {
+            Swal.fire(
+              "ocurrio un error",
+              "intente esta operacion mas tarde",
+              "error"
+            );
+          }
+        });
+
+
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
     })
-  }
-
-
-
+   
+  };
 
   return (
     <>
@@ -32,18 +57,20 @@ const ItemProducto = ({producto,setProductos}) => {
         <td>${producto.precio}</td>
         <td>{producto.imagen}</td>
         <td>{producto.categoria}</td>
-        <td><Button 
-        className="my-1" 
-        as={Link} 
-        to="/editarProducto"
-        variant="warning"
-        >Editar</Button>
-        <Button 
-        onClick={borrarProducto}
-        variant="danger"
-        >Borrar</Button></td>
+        <td>
+          <Button
+            className="my-1"
+            as={Link}
+            to="/editarProducto"
+            variant="warning"
+          >
+            Editar
+          </Button>
+          <Button onClick={borrarProducto} variant="danger">
+            Borrar
+          </Button>
+        </td>
       </tr>
-     
     </>
   );
 };
